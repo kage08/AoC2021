d596186510f13b0c7ee24dbf61171b017bf6b05c
kage08 <harshavardhan864.hk@gmail.com>
2021-12-25 16:00:52 -0500

    Format utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,10 @@
-
-
 use std::fs;
-    pub fn parsefile(filename: &str) -> Vec<String> {
-        let lines = fs::read_to_string(filename).expect("Unable to read file");
-        let linevec = lines
-            .split("\n")
-            .filter(|line| line.len() > 0)
-            .map(|line| line.to_string())
-            .collect::<Vec<String>>();
-        linevec
-    }
-
+pub fn parsefile(filename: &str) -> Vec<String> {
+    let lines = fs::read_to_string(filename).expect("Unable to read file");
+    let linevec = lines
+        .split("\n")
+        .filter(|line| line.len() > 0)
+        .map(|line| line.to_string())
+        .collect::<Vec<String>>();
+    linevec
+}
